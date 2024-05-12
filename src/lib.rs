8fa4272fd3d506e634c280ece9d873a1aaa83a75
rosefarts <gonesbird@gmail.com>
2024-05-12 13:51:34 +0200

    features

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub use bevy_event_entities_core::*;
 
+#[cfg(feature = "event_listener")]
 pub mod event_listener {
     pub use bevy_event_entities_derive::Listenable;
     pub use bevy_event_entities_listener::*;
@@ -7,10 +8,13 @@ pub mod event_listener {
 
 pub mod prelude {
     pub use bevy_event_entities_core::prelude::*;
+    #[cfg(feature = "derive")]
     pub use bevy_event_entities_derive::*;
+    #[cfg(feature = "event_listener")]
     pub use bevy_event_entities_listener::prelude::*;
 }
 
+#[cfg(feature = "derive")]
 pub mod derive_exports {
     pub use bevy_ecs::world::EntityRef; // required for `#[derive(Listenable)]`
 }
